5e06681bc7c06e4a0756d0d706ac9237db0fe27b
Matheuzinho <grandboss1978@gmail.com>
2025-04-29 12:00:11 -0300

    Atualização Exercicios 3.c

--- Avaliação 8/Exercicios 3.c
+++ Avaliação 8/Exercicios 3.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <math.h> 
 
-// Função que calcula a raiz quadrada da soma dos quadrados de dois valores reais
 double raiz(double a, double b) {
     return sqrt(a * a + b * b);
 }
@@ -12,15 +11,13 @@ int main() {
 	setlocale(LC_ALL, "Portuguese");
     double num1, num2;
     
-    // Solicita ao usuário para digitar dois valores reais
     printf("Digite o primeiro valor real: ");
     scanf("%lf", &num1);
     printf("Digite o segundo valor real: ");
     scanf("%lf", &num2);
     
-    // Calcula e exibe o resultado
     double resultado = raiz (num1, num2);
-    printf("A raiz quadrada da soma dos quadrados é: %.2f\n", resultado);
+    printf("A raiz quadrada da soma dos quadrados Ã©: %.2f\n", resultado);
     
     return 0;
 }
