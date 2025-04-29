98520ccc16711eaeefcac6cacabc1b86fdd87335
Matheuzinho <grandboss1978@gmail.com>
2025-04-29 11:59:23 -0300

    Atuaalização Exercicios 12.c

--- Avaliação 8/Exercicios 12.c
+++ Avaliação 8/Exercicios 12.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// Função para verificar se um ano é bissexto
 bool anoBissexto(int ano) {
     return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
 }
 
-// Função para verificar se uma data é válida
 bool dataValida(int ano, int mes, int dia) {
-    // Verifica o intervalo do ano
     if (ano < 1900) return false;
-
-    // Verifica o intervalo do mês
+    
     if (mes < 1 || mes > 12) return false;
-
-    // Verifica o intervalo do dia, considerando os dias de cada mês
+    
     if (dia < 1) return false;
 
     int diasNoMes;
@@ -32,19 +27,17 @@ bool dataValida(int ano, int mes, int dia) {
 int main() {
     int ano, mes, dia;
 
-    // Solicita ao usuário para digitar a data
     printf("Digite o ano: ");
     scanf("%d", &ano);
-    printf("Digite o mês: ");
+    printf("Digite o mÃªs: ");
     scanf("%d", &mes);
     printf("Digite o dia: ");
     scanf("%d", &dia);
 
-    // Verifica se a data é válida e exibe o resultado
     if (dataValida(ano, mes, dia)) {
-        printf("A data %d/%d/%d é válida.\n", dia, mes, ano);
+        printf("A data %d/%d/%d Ã© vÃ¡lida.\n", dia, mes, ano);
     } else {
-        printf("A data %d/%d/%d não é válida.\n", dia, mes, ano);
+        printf("A data %d/%d/%d nÃ£o Ã© vÃ¡lida.\n", dia, mes, ano);
     }
 
     return 0;
